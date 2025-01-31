c3da83f02493cf0b962cba99bbf4aa18d9616473
melisesilveira <melisechaves@gmail.com>
2025-01-31 11:54:49 -0300

    teste2-acineto

--- src/utils/handle_processing.py
+++ src/utils/handle_processing.py
@@ -282,7 +282,7 @@ def handle_species(species_info: SpeciesDict, species_data: dict) -> \
         #print_species = acineto.get("display_name") 
         mlst_species = acineto.get("mlst")
 
-        return None, print_species, mlst_species
+        return None, mlst_species
     elif "enterobacter" in species:
         entero = species_data.get("enterobacter_species", {})
         print_species = entero.get("display_name")
